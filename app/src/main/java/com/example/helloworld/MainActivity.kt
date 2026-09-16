05ffc836d80319cbc5b3e59c106099f22c535726
techwave453-del <techwave453@gmail.com>
2026-09-16 07:07:58 +0300

    Fix Compose splash image import

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -19,7 +19,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.clip
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
