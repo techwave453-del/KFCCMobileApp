4bf9f2b53ed3c1d4cfa7e9e73606acdff2367d76
techwave453-del <techwave453@gmail.com>
2026-09-16 07:17:28 +0300

    Introduce KFCC modern brand color palette

--- app/src/main/java/com/example/helloworld/ui/theme/Color.kt
+++ app/src/main/java/com/example/helloworld/ui/theme/Color.kt
@@ -2,10 +2,20 @@ package com.example.helloworld.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val KfccNavy = Color(0xFF10233F)
+val KfccBlue = Color(0xFF1F5FAF)
+val KfccSky = Color(0xFFEAF3FF)
+val KfccGold = Color(0xFFD9A441)
+val KfccInk = Color(0xFF182230)
+val KfccMuted = Color(0xFF667085)
+val KfccSurface = Color(0xFFF7F9FC)
+val KfccWhite = Color(0xFFFFFFFF)
+val KfccDarkSurface = Color(0xFF101722)
+val KfccDarkCard = Color(0xFF182231)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple80 = KfccSky
+val PurpleGrey80 = Color(0xFFC7D4E8)
+val Pink80 = Color(0xFFF1D9A6)
+val Purple40 = KfccBlue
+val PurpleGrey40 = KfccNavy
+val Pink40 = KfccGold
