c12f662f75935dec12dd1b2d8acccc4274a640dc
techwave453-del <techwave453@gmail.com>
2026-09-16 06:09:09 +0300

    Fix multipart headers import

--- app/src/main/java/com/example/helloworld/admin/AdminRepository.kt
+++ app/src/main/java/com/example/helloworld/admin/AdminRepository.kt
@@ -15,10 +15,11 @@ import io.ktor.client.request.patch
 import io.ktor.client.request.post
 import io.ktor.client.request.put
 import io.ktor.client.request.setBody
-import io.ktor.client.request.forms.formData
 import io.ktor.client.request.forms.MultiPartFormDataContent
+import io.ktor.client.request.forms.formData
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.ContentType
+import io.ktor.http.Headers
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
 import io.ktor.serialization.kotlinx.json.json
