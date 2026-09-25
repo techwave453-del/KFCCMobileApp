a11280eafaeeeb27c1de04dde8452c12c5f86225
techwave453-del <techwave453@gmail.com>
2026-09-25 17:58:29 +0300

    Fix missing runBlocking import in application startup

--- app/src/main/java/com/example/helloworld/KfccApplication.kt
+++ app/src/main/java/com/example/helloworld/KfccApplication.kt
@@ -12,6 +12,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class KfccApplication : Application() {
     override fun onCreate() {
